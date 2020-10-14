4cba358161374dfbc420b9cbec0af0972d377f49
YYCOCO <1428275037@qq.com>
2020-10-14 08:46:22 +0800

    李光远提交

--- GitStudy/src/main/Lgy.java
+++ GitStudy/src/main/Lgy.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 
 public class Lgy {
 void out(ArrayList arr) {
-		
+	 int max=(int) arr.get(0);  
+	 int min=(int) arr.get(0);
+	 
+	 for(int i =0;i<arr.size();i++){
+		 if(min>(int)arr.get(i)) min = (int)arr.get(i);
+		 if(max<(int)arr.get(i)) max = (int)arr.get(i);
+	 }
+	 System.out.println("max为"+max+"\n"+"min为"+min);
 	}
 
 }
